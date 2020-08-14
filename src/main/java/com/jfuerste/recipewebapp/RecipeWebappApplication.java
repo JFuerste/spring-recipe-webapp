ea91f1fc957a54f5b4175e1b8f22b7e4061b40a7
Fürste <joshua.fuerste@rwth-aachen.de>
2020-08-14 16:14:15 +0200

    Initial commit

---
+++ src/main/java/com/jfuerste/recipewebapp/RecipeWebappApplication.java
@@ -0,0 +1,13 @@
+package com.jfuerste.recipewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jfuerste/recipewebapp/RecipeWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfuerste.recipewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
