6912038fdf632b476d2abb04be56c0015fff58c3
Fürste <joshua.fuerste@rwth-aachen.de>
2020-08-16 13:37:36 +0200

    Add UnitOfMeasure entity to Ingredient

--- src/main/java/com/jfuerste/recipewebapp/domain/Ingredient.java
+++ src/main/java/com/jfuerste/recipewebapp/domain/Ingredient.java
@@ -12,6 +12,10 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @OneToOne
+    private UnitOfMeasure uom;
+
+
     @ManyToOne
     private Recipe recipe;
 
@@ -47,5 +51,11 @@ public class Ingredient {
         this.recipe = recipe;
     }
 
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
 
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
 }
