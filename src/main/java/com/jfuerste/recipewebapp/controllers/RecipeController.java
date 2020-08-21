efefb41b89891503ab6828b422bce1e8f5abeaf1
Fürste <joshua.fuerste@rwth-aachen.de>
2020-08-21 16:41:10 +0200

    Add form to update recipes

--- src/main/java/com/jfuerste/recipewebapp/controllers/RecipeController.java
+++ src/main/java/com/jfuerste/recipewebapp/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.jfuerste.recipewebapp.controllers;
 
+import com.jfuerste.recipewebapp.commands.RecipeCommand;
 import com.jfuerste.recipewebapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,10 +19,24 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String updateRecipe(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
