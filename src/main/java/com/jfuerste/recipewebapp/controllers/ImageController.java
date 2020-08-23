2959c3535a7297975da3a3a754a56217ffa4eb29
Fürste <joshua.fuerste@rwth-aachen.de>
2020-08-23 14:17:24 +0200

    Add custom image rendering

--- src/main/java/com/jfuerste/recipewebapp/controllers/ImageController.java
+++ src/main/java/com/jfuerste/recipewebapp/controllers/ImageController.java
@@ -1,7 +1,9 @@
 package com.jfuerste.recipewebapp.controllers;
 
+import com.jfuerste.recipewebapp.commands.RecipeCommand;
 import com.jfuerste.recipewebapp.services.ImageService;
 import com.jfuerste.recipewebapp.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageController {
@@ -36,6 +43,22 @@ public class ImageController {
 
         return "redirect:/recipe/" + id + "/show";
 
+    }
+
+    @GetMapping("recipe/{id}/recipeImage")
+    public void renderImage(@PathVariable String id, HttpServletResponse response) throws IOException {
+        RecipeCommand command = recipeService.findCommandById(Long.valueOf(id));
+        byte[] rawByteArray = new byte[command.getImage().length];
+
+        int i = 0;
+        for (Byte b : command.getImage()){
+            rawByteArray[i] = b;
+            i++;
+        }
+
+        response.setContentType("image/jpeg");
+        InputStream is = new ByteArrayInputStream(rawByteArray);
+        IOUtils.copy(is, response.getOutputStream());
     }
 
 }
--- src/test/java/com/jfuerste/recipewebapp/controllers/ImageControllerTest.java
+++ src/test/java/com/jfuerste/recipewebapp/controllers/ImageControllerTest.java
@@ -7,10 +7,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
@@ -64,5 +66,31 @@ class ImageControllerTest {
                 .andExpect(header().string("Location", "/recipe/1/show"));
 
         verify(imageService).saveImageFile(anyLong(), any());
+    }
+
+    @Test
+    void renderImage() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(1L);
+
+        String s = "testtext";
+        Byte[] byteArray = new Byte[s.getBytes().length];
+
+        int i = 0;
+        for (byte b : s.getBytes()){
+            byteArray[i] = b;
+            i++;
+        }
+        command.setImage(byteArray);
+
+        when(recipeService.findCommandById(anyLong())).thenReturn(command);
+
+        MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeImage"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+
+        byte[] responseBytes = response.getContentAsByteArray();
+
+        assertEquals(s.getBytes().length, responseBytes.length);
     }
 }
